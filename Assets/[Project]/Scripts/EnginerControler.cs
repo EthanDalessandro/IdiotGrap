6bddd6bcb58ef014fa0b83c7b6df70d5703d5ee0
Arthur Caumartin <artcaumart@gmail.com>
2024-04-26 13:34:08 +0200

    Update EnginerControler.cs

--- Assets/[Project]/Scripts/EnginerControler.cs
+++ Assets/[Project]/Scripts/EnginerControler.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class EnginerControler : MonoBehaviour
 {
     [SerializeField] private float _speed = 5f;
+    private Vector2 _inputVector;
 
     private Rigidbody _rigidbody;
 
@@ -14,11 +15,17 @@ public class EnginerControler : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody>();
     }
 
+    void FixedUpdate()
+    {
+        transform.forward = new Vector3(_inputVector.x, 0, _inputVector.y);
 
+        Vector3 newVelocity = transform.forward * _inputVector.magnitude * _speed;
+        newVelocity.y = _rigidbody.velocity.y;
+        _rigidbody.velocity = newVelocity;
+    }
 
     private void OnMove(InputValue value)
     {
-        Vector2 inputVector = value.Get<Vector2>();
-        _rigidbody.velocity = new Vector3(inputVector.x, 0, inputVector.y) * _speed;
+        _inputVector = value.Get<Vector2>();
     }
 }
